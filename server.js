6a8be1d5b27b4cd7147b291c6a5a26ecfac16cfb
rahulchauhan04 <rahulg0004@gmail.com>
2024-06-08 02:04:35 +0530

    chat app BE

--- server.js
+++ server.js
@@ -1,20 +1,18 @@
-const express = require('express');
-const http = require('http');
-const socketIo = require('socket.io');
+const express = require("express");
+const http = require("http");
+const socketIo = require("socket.io");
 const socketIoServer = socketIo.Server;
 
-const app = express();
+const app = express(); 
 app.use(express.static('frontend'));
 
-const httpVersionOfApp = http.createServer(app);
+const httpVersionOfApp = http.createServer(app); // socket io works with 
+
 const io = new socketIoServer(httpVersionOfApp);
+io.on('connection',(socket)=>{
+    socket.on('sending message',(data)=>{
+        io.emit('io spreading message',data);
+    })
+})
 
-io.on('connection', (socket) => {
-     socket.io('sending message', (data) => {
-          io.emit('io spreading message', data);
-     });
-});
-
-// app.get('/', (req, res) => res.send('Hello World!'));
-
-app.listen(3000);
+httpVersionOfApp.listen(3000);
